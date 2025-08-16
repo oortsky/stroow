b27c94889602e95879d8a01d89ccb63a3b433beb
oortsky <oortsky@proton.me>
2025-08-16 14:29:39 +0700

    Init & Setup Decap CMS - Troow [16/08/2025]

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,6 +2,6 @@ declare module "*.md" {
   import * as React from "react";
   const MDXComponent: React.FC;
 
-  export const attributes: Record<string, any>;
+  export const attributes: Record<string, unknown>;
   export default MDXComponent;
 }
